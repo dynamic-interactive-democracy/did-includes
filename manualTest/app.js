282d8ad3c3b02f8ce05cab11bd6c29c11f8876a4
Niels Abildgaard <niels.abildgaard@gmail.com>
2017-08-24 23:41:11 +0200

    Simpler config-based (rather than file system-based) structure of manual test pages
    This makes it much easier to get an overview of the tests that are set up, how they are configured, and how to add more.

--- manualTest/app.js
+++ manualTest/app.js
@@ -1,18 +1,63 @@
 const express = require("express");
 const path = require("path");
 const staticView = require("./staticView");
-const circleTestsApp = require("./circles/app");
-const topicsTestsApp = require("./topics/app");
-const tasksTestsApp = require("./tasks/app");
+const includeTestView = require("./includeTestView/fun");
 
 module.exports = (config) => {
     let app = express();
 
     app.get("/", staticView(path.join(__dirname, "index", "view.html")));
     app.use("/assets", express.static(path.join(__dirname, "assets")));
-    app.use("/circles", circleTestsApp(config));
-    app.use("/topics", topicsTestsApp(config));
-    app.use("/tasks", tasksTestsApp(config));
+
+    setUpTestPages(app, config, {
+        circles: {
+            create: { include: "circleCreate", args: { fill: {
+                title: "Imported Title",
+                aim: "We aim to import this text.",
+                fullState: "full",
+                invite: [ "user-id-002" ]
+            }}},
+            view: { include: "circleView", args: { id: "__from_query" } },
+            edit: { include: "circleEdit", args: { id: "__from_query" } }
+        },
+        topics: {
+            create: { include: "topicCreate", args: { circleId: "__from_query" } },
+            edit: { include: "topicEdit", args: { id: "__from_query", circleId: "__from_query" } },
+            view: { include: "topicView", args: { id: "__from_query", circleId: "__from_query" } }
+        },
+        tasks: {
+            create: { include: "taskCreate", args: { circleId: "__from_query" } },
+            edit: { include: "taskEdit", args: { id: "__from_query", circleId: "__from_query" } },
+            view: { include: "taskView", args: { id: "__from_query", circleId: "__from_query" } }
+        }
+    });
 
     return app;
 };
+
+function setUpTestPages(mainApp, appConfig, pagesConfig) {
+    Object.keys(pagesConfig).forEach(category => {
+        let app = express();
+
+        Object.keys(pagesConfig[category]).forEach(page => {
+            app.get(`/${page}`, includeTestView(appConfig, `${category}/${page}`, createFunctionString(pagesConfig[category][page])));
+        });
+
+        mainApp.use(`/${category}`, app);
+    });
+}
+
+function createFunctionString(config) {
+    return `did.includes.${config.include}(${renderArgs(config.args)})`;
+}
+
+function renderArgs(args) {
+    return "{ " + Object.keys(args).map(key => `${key}: ${renderArg(args, key)}`).join(", ") + " }";
+}
+
+function renderArg(args, key) {
+    if(args[key] == "__from_query") {
+        return `getQueryParam(${JSON.stringify(key)})`;
+    }
+    return JSON.stringify(args[key]);
+}
--- manualTest/includeTestView/fun.js
+++ manualTest/includeTestView/fun.js
@@ -3,11 +3,12 @@ const fs = require("fs");
 const path = require("path");
 const view = fs.readFileSync(path.join(__dirname, "view.html"), "utf8");
 
-module.exports = (config, setUpFun) => {
+module.exports = (config, title, includeFun) => {
     let result = mustache.render(view, {
         apiUrl: config.apiUrl,
         currentUser: config.currentUser,
-        setUpFun: setUpFun.toString()
+        title: title,
+        includeFun: includeFun.toString()
     });
 
     return (req, res) => res.send(result);
